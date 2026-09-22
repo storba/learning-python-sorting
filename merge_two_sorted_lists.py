b9cbdb67d0afe3f518bfc77b69f772d155e896fd
Svetlana <storba@gmail.com>
2026-09-22 18:57:03 +0200

    add check with random arrays

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -1,4 +1,6 @@
 import time
+import random
+
 def timed(func):
     def wrapper(*args, **kwargs):
         start = time.perf_counter()
@@ -32,9 +34,21 @@ def shadow_merge(list1: list[int], list2: list[int]) -> list[int]:
 def shadow_merge_2(list1: list[int], list2: list[int]) -> list[int]:
     return sorted(list1 + list2)
 
+def random_sorted(n, low=0, high=1000):
+    arr = [random.randint(low, high) for _ in range(n)]
+    arr.sort()
+    return arr
+
 if __name__ == "__main__":
     evens = [2 * i for i in range(10000)]
     odds = [2 * i + 1 for i in range(10000)]
 
+    print("evens and odds")
     shadow_merge(evens, odds)
     shadow_merge_2(evens, odds)
+
+    a = random_sorted(10000)
+    b = random_sorted(10000)
+    print("random sorted")
+    shadow_merge(a, b)
+    shadow_merge_2(a, b)
