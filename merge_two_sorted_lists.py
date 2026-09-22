d9a0b0d65a2b4f6efcd43ac77e731481b12a3503
Svetlana <storba@gmail.com>
2026-09-22 10:47:31 +0200

    rm comments

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -38,10 +38,3 @@ if __name__ == "__main__":
 
     shadow_merge(evens, odds)
     shadow_merge_2(evens, odds)
-    # print(shadow_merge([1,3,5], [2,4,6]))
-    # print(shadow_merge([1,2,3], [4,5,6]))
-    # print(shadow_merge([1], [2,3,4]))
-    # print(shadow_merge([], [1,2,3]))
-    # print(shadow_merge([1,1,2], [1,3,3]))
-    # print(shadow_merge([1,1,2], []))
-    # print(shadow_merge([], []))
